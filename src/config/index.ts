877b874b2e3868ab05a657b18fa2da08d63dc9b0
maximegiguere1one <maxime@agence1.com>
2025-10-30 19:25:54 -0400

    Updated index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,8 @@
+import { APP_CONFIG } from './app.config';
+import { TIMING_CONFIG } from './timing.config';
+import { BUSINESS_CONFIG } from './business.config';
+import { UI_CONFIG } from './ui.config';
+
 export { APP_CONFIG, type AppConfig } from './app.config';
 export { TIMING_CONFIG, type TimingConfig } from './timing.config';
 export { BUSINESS_CONFIG, type BusinessConfig } from './business.config';
